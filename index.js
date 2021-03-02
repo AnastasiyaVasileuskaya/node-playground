c6eadb2149f28ed6fad2a09579b74db87bce2028
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-03-02 16:40:35 +0100

    Add countlinesadvanced

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const hello = require('./hello')
+/*const hello = require('./hello')
 hello()
 
 const random = require('./random')
-
 const max = Number(process.argv[2])
 console.log(random(max))
+*/
+
+const read = require('./read.js')
+read('.prettierrc')
---
+++ write.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const { exit } = require('process')
+
+const file = 'tools.json'
+
+const tool = process.argv[2]
+
+//example from fetch:
+// fetch(url)
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+fs.promises
+  .readFile(file, { encoding: 'utf-8' })
+  .then(addToolToJSON)
+  .then(onWriteSuccess)
+  .catch(onError)
+
+function addToolToJSON(data) {
+  const tools = JSON.parse(data)
+  tools.push({ name: tool })
+  return fs.promises.writeFile(file, JSON.stringify(tools), {
+    encoding: 'utf-8',
+  })
+}
+
+function onWriteSuccess() {
+  exit(0)
+}
+
+function onError(error) {
+  console.error(error)
+}
+//old style with nested callbacks:
+/*
+fs.readFile(file, { encoding: 'utf-8' }, (err, data) => {
+  err && exit(1)
+  const tools = JSON.parse(data)
+  tools.push({ name: tool })
+})
+
+fs.writeFile(file, JSON.stringify(data), { encoding: 'utf-8' }, err => {
+  exit(err ? 1 : 0)
+})
+*/
